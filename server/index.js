be2f614c2e709fa1eb539f36a303324481454e0f
tuancuongngo <70221527+tuancuongngo@users.noreply.github.com>
2023-04-13 20:12:51 -0400

    Add MongoDB and Mongoose config

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 
+// MIDDLEWARE TO PARSE JSON DATA to JS object accessible in req.body
 app.use(express.json());
+
+// Load environment variables from .env file
+require("dotenv").config();
+
+// DATABASE
+const mongoose = require("mongoose");
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log(`[SUCCESS] MongoDB database connection established successfully`);
+});
 
 const port = process.env.PORT || 4444;
 app.listen(port, () => {
