26fcfa36eaad969d28d0a98d87b8ed596fe2dd90
tuancuongngo <70221527+tuancuongngo@users.noreply.github.com>
2023-04-15 20:49:46 -0400

    Add CSS Styling for TODO list components

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>Welcome back</h1>
+            <h4>Current TODO List</h4>
+            <div className="todoArea">
+                <div className="task">
+                    {/* <div className="checkbox"></div> */}
+                    <div className="taskName">Finish MERN by end of today</div>
+                    <div className="deleteBtn">
+                        <span class="fas fa-trash-alt"></span>
+                    </div>
+                </div>
+                <div className="task isComplete">
+                    {/* <div className="checkbox"></div> */}
+                    <div className="taskName">Yes</div>
+                    <div className="deleteBtn">
+                        <span class="fas fa-trash-alt"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
