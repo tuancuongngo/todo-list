66a1c547f95a618394a9a22c695e8b99edd8445c
tuancuongngo <70221527+tuancuongngo@users.noreply.github.com>
2023-04-16 13:28:56 -0400

    Add POST requests to add new TODO tasks to MongoDB and update frontend

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const taskDeleted = (taskName) => {
 function App() {
     const [tasks, setTasks] = useState([]);
     // const [popupToggle, setPopupToggle] = useState(false);
-    // const [newTask, setNewTask] = useState("");
+    const [taskInput, setTaskInput] = useState("");
 
     useEffect(() => {
         const getTasks = async () => {
@@ -33,11 +33,41 @@ function App() {
         getTasks();
     }, []);
 
+    const addTask = async (event) => {
+        console.log(taskInput);
+        event.preventDefault();
+
+        const newTask = {
+            taskName: taskInput,
+            completed: false,
+            timeStamp: "test",
+        };
+
+        try {
+            const response = await axios.post("http://localhost:4444/api/tasks", newTask);
+            setTasks([...tasks, response.data]);
+            setTaskInput("");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    function handleChange(event) {
+        const newValue = event.target.value;
+        setTaskInput(newValue);
+    }
+
     return (
         <div className="App">
             <ToastContainer position="top-right" theme="colored" hideProgressBar={false} autoClose={2000} />
             <h1>Welcome back</h1>
             <h4>Current TODO List</h4>
+            <div className="taskForm">
+                <input onChange={handleChange} type="text" value={taskInput} />
+                <button onClick={addTask}>
+                    <span>Add</span>
+                </button>
+            </div>
             <div className="todoArea">
                 {tasks.map((task) => (
                     <Task
